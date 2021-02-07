f694ecca60e30f13506b83b14bfbe91abaa95868
Siaj <sibdowsaij@gmail.com>
2021-02-07 02:57:28 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p><h1>Hello from React.js</h1></p>
+        <h2>My name is Sibdow Iddrisu</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
